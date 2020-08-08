a1968a368da29e55e0487d4e031f7ad7707c3319
Shengjie Yan <ysjshengjie@live.cn>
2020-08-09 07:13:44 +0800

    add apollo nuget package

---
+++ K8S.NET.Apollo/Controllers/ApolloController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace K8S.NET.Apollo.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class ApolloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetString(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult GetSection(string sectionKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult GetJson(string jsonKey)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
