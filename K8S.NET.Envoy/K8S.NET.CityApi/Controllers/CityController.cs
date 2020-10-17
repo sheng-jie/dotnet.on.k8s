1d69710aec4ac38d5e7f253f2b1546ed42f59f36
shengjie <ysjshengjie@live.cn>
2020-10-17 16:36:18 +0800

    add envoy demo

---
+++ K8S.NET.Envoy/K8S.NET.CityApi/Controllers/CityController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace K8S.NET.CityApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CityController : ControllerBase
+    {
+        private static readonly string[] Cities = new[]
+        {
+            "Beijing", "Shanghai", "Shenzhen", "Wuhan", "Hong Kong", "London"
+        };
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var rng = new Random();
+            
+            return Ok(Cities[rng.Next(Cities.Length)]);
+        }
+    }
+}
---
+++ K8S.NET.Envoy/K8S.NET.WeatherApi/Controllers/WeatherController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace K8S.NET.WeatherApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var rng = new Random();
+            
+            return Ok(Summaries[rng.Next(Summaries.Length)]);
+        }
+    }
+}
